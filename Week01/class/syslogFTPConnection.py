3d10bca10fdd5478a7f1f5d642bdaf8a91625fb9
cdb22 <cole.davisbrand@mymail.champlain.edu>
2022-08-31 16:37:13 -0400

    classwork 1 complete

---
+++ Week01/class/syslogFTPConnection.py
@@ -0,0 +1,30 @@
+import syslogCheck
+import importlib
+importlib.reload(syslogCheck)
+
+# SSH authentication failures
+def ftp_connects(filename, searchTerms):
+    
+    # Call syslogCheck and return the results
+    is_found = syslogCheck._syslog(filename, searchTerms)
+
+    # Found list
+    found = []
+
+    # Loop through the results
+    for eachFound in is_found:
+        
+        #print(eachFound)
+        # Split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split value to the found list
+        found.append(sp_results[3])
+
+    # Remove duplicates by using set
+    # and convert the list to a dictionary
+    hosts = set(found)
+
+    # Print results
+    for eachHost in hosts:
+        print(eachHost)
