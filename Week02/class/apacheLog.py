5b3e1fbf40ef83eaf16f0d96529d075560127e92
cdb22 <cole.davisbrand@mymail.champlain.edu>
2022-09-07 11:48:11 -0400

    classwork 2 complete

---
+++ Week02/class/apacheLog.py
@@ -0,0 +1,31 @@
+import logCheck
+import importlib
+importlib.reload(logCheck)
+
+# SSH authentication failures
+def apache_events(filename, service, term):
+    
+    # Call syslogCheck and return the results
+    is_found = logCheck._logs(filename, service, term)
+
+    # Found list
+    found = []
+
+    # Loop through the results
+    for eachFound in is_found:
+        
+        #print(eachFound)
+        # Split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split value to the found list
+        # "GET /server-info?mod_info.c HTTP/1.1" 404 435 "-" "-"
+        found.append(sp_results[3] + " " + sp_results[0] + " " + sp_results[1])
+
+    # Remove duplicates by using set
+    # and convert the list to a dictionary
+    hosts = set(found)
+
+    # Print results
+    for eachHost in hosts:
+        print(eachHost)
